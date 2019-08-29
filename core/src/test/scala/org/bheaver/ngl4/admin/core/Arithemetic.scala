7d52e41e612667eac01961cdb269dca677ff6600
Bagi Errabolu <errabolu.bagi@gmail.com>
2019-08-29 17:47:48 -0400

    Added Arithemetic

---
+++ core/src/test/scala/org/bheaver/ngl4/admin/core/Arithemetic.scala
@@ -0,0 +1,29 @@
+package org.bheaver.ngl4.admin.core
+
+class Arithemetic {
+
+  def add(a:Int,b:Int): Unit ={
+    var sum=a+b
+    println(sum)
+  }
+  def sub(a:Int,b:Int): Unit ={
+    var sum=a+b
+    println(sum)
+  }
+  def mul(a:Int,b:Int): Unit ={
+    var sum=a+b
+    println(sum)
+  }
+  def div(a:Int,b:Int): Unit ={
+    var sum=a+b
+    println(sum)
+  }
+}
+object  MainObject{
+  def main (args: Array[String] ): Unit = {
+    var a = new Arithemetic()
+    a.add(10,20)
+
+}
+
+}
