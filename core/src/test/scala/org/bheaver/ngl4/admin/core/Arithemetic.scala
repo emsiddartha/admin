b9318e49d4ba66df97c485db9f809e95ecaf328b
Bagi Errabolu <errabolu.bagi@gmail.com>
2019-08-29 19:30:59 -0400

    Modify Sub

--- core/src/test/scala/org/bheaver/ngl4/admin/core/Arithemetic.scala
+++ core/src/test/scala/org/bheaver/ngl4/admin/core/Arithemetic.scala
@@ -7,11 +7,11 @@ class Arithemetic {
     println(sum)
   }
   def sub(a:Int,b:Int): Unit ={
-    var sum=a+b
-    println(sum)
+    var sub=a-b
+    println(sub)
   }
   def mul(a:Int,b:Int): Unit ={
-    var sum=a+b
+    var sum=a*b
     println(sum)
   }
   def div(a:Int,b:Int): Unit ={
@@ -23,6 +23,7 @@ object  MainObject{
   def main (args: Array[String] ): Unit = {
     var a = new Arithemetic()
     a.add(10,20)
+    a.sub(10,20)
 
 }
 
